ccb7abfcbcc883b8d9251aad08a1efcf12c6c49c
happyheng <happyheng9522@163.com>
2018-11-10 17:57:52 +0800

    去掉master节点

--- src/main/java/com/happyheng/lock/LockMaster.java
+++ src/main/java/com/happyheng/lock/LockMaster.java
@@ -20,30 +20,5 @@ public class LockMaster {
     public void init() {
 
 
-//            // 启动时即监听对应 master 节点
-//            boolean createSuccess = false;
-//            try {
-//                // 创建 /lock_getGood
-//                zooKeeper.create(LockConsts.LOCK_MASTER_PATH + "/master", "".getBytes(), OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-//                createSuccess = true;
-//            } catch (KeeperException.NodeExistsException e) {
-//
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            } catch (KeeperException e) {
-//                e.printStackTrace();
-//            }
-//
-//            if (createSuccess) {
-//
-//                new Thread(()->{
-//                    Node node = new MasterNode(zooKeeper);
-//                    node.doJob();
-//                }).start();
-//
-//            }
-
-
-
     }
 }
